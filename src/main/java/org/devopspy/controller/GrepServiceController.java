52f7b84cc1450513462e1a052eb5740b660db3da
marcocast <marcocast@gmail.com>
2014-02-27 23:08:13 +0000

    fix api grep rest path

--- src/main/java/org/devopspy/controller/GrepServiceController.java
+++ src/main/java/org/devopspy/controller/GrepServiceController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping(value = "/")
+@RequestMapping(value = "/api/")
 public class GrepServiceController {
 
 	@Inject
